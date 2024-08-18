963b58266e8d40458666c2f942f3eee2557ad979
polilotas123 <layyagami9@gmail.com>
2024-08-17 20:55:24 -0500

    init screen

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <QApplication>
+#include <QGraphicsScene>
+#include <QGraphicsRectItem>
+#include <QGraphicsView>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    QGraphicsScene * scene = new QGraphicsScene();
+    QGraphicsRectItem * rect = new QGraphicsRectItem();
+    rect->setRect(0,0, 100, 100);
+
+    scene->addItem(rect);
+
+    QGraphicsView * view = new QGraphicsView();
+    view->setScene(scene);
+
+    view->show();
+    return a.exec();
+}
